fbe9b458eeea60dac5060ae77240361520323aba
EB <esabil@shipshuk.com>
2019-09-17 22:21:15 +0300

    switched back to master on nuc

---
+++ src/bb/app/dekonts/DekontSummary.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bb.app.dekonts;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class DekontSummary
+{
+    public ArrayList<MonthSummary> MonthsLastReport;
+    public ArrayList<MonthSummary> MonthsAllReports;
+
+
+    public DekontSummary()
+    {
+        MonthsLastReport = new ArrayList<MonthSummary>();
+        MonthsAllReports = new ArrayList<MonthSummary>();
+    }
+}
+
+
